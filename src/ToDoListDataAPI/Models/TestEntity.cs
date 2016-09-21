4c88d6ada6832d094fd7ad149a93a23d0794fada
Bob Jacobs <jacobsrj@hotmail.com>
2016-09-20 21:09:10 -0400

    First code checkin

---
+++ src/ToDoListDataAPI/Models/TestEntity.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToDoListDataAPI.Models
+{
+    public class TestEntity : TableEntity
+    {
+        public TestEntity(string description, string owner)
+        {
+            this.PartitionKey = owner;
+            this.RowKey = description;
+        }
+
+        public TestEntity() { }
+
+        public string AdditionalInformation { get; set; }
+    }
+}
